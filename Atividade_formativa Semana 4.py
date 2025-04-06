2e11d98e42ed8a70adefc748c585c92048e1415b
gugahali <halickigustavo@gmail.com>
2025-04-06 15:57:13 -0300

    Adicionei a Base de Dados(SQLite) e Realizei o CRUD

--- Atividade_formativa Semana 4.py
+++ Atividade_formativa Semana 4.py
@@ -1,79 +1,114 @@
-'''
+import sqlite3
 
-Nome: Gustavo Halicki
-Curso: Analise e Desenvolvimento de Sistemas (ADS)
+conexao = sqlite3.connect('Estudantes.db')
+cursor = conexao.cursor()
 
-'''
-#Criação de uma Matriz contendo 5 listas vazias e adicionando na variavel "faculdade"
+#Criação da Tabela Dos Dados
+#cursor.execute (''' CREATE TABLE Estudantes (
+#                    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+#                    nome TEXT NOT NULL);''')    
+
+
 faculdade = [[],[],[],[],[]]
 
-#Começa uma Estrutura de repetição com While True
 while True :
-    #Pergunta ao usuario qual das 5 opções do Menu Principal ele pretende acessar, Guardando a Resposta na variavel "menu_princ"
-    menu_princ = input(("------ Menu Principal -----\n (1) Verificar Estudantes \n (2) Verificar Professores \n (3) Verificar Disciplinas \n (4) Verificar Turmas \n (5) Verificar Matriculas \n (8) Sair \n Opção Selecionada: "))
+    menu_princ = int(input("------ Menu Principal -----\n (1) Verificar Estudantes \n (2) Verificar Professores \n (3) Verificar Disciplinas \n (4) Verificar Turmas \n (5) Verificar Matriculas \n (8) Sair \n Opção Selecionada: "))
     
-    #Verifica se o valor da variavel "menu_princ" é igual a '1', utilizando o if junto de uma estrutura de condição 
-    if (menu_princ == '1'):
+    if (menu_princ) not in [1,2,3,4,5,0] :
+        print('\n Por favor, Insira um Número do Menu Principal\n')
+      
+
+    if (menu_princ == 1):
         while True:
-            #Pergunta ao Usuario qual das 5 opções do menu de operações ele deseja acessar,e guarda o resultado na variavel "opção"
-            opção = input(' \n ----[Estudantes]---- Menu de Modificações \n (1) Incluir \n (2) Listar \n (3) Atualizar \n (4) Excluir \n (0) Voltar ao Menu Principal \n Opção Selecionada: ') 
-            #Verifica se o valor da variavel "opção" é igual a '1', utilizando o if junto de uma estrutura de condição
-            if opção == '1':
-                print('---Incluir Estudante---')
-                #Pergunta ao usuario qual o nome do estudante que vai ser adicionada a lista, e guarda na variavel estudante_sendo_adicionado
-                estudante_sendo_adicionado = str(input('Qual Seria o Nome Do Estudante?'))
-                #Adiciona o Valor da variavel estudante_sendo_adicionado, adicionando na lista[0] , utilizando o metodo append
-                faculdade[0].append(estudante_sendo_adicionado)
-                #Mostra que o Estudante foi adicionado a lista, junto do nome do mesmo
-                print('Estudante {} Adicionado'.format(estudante_sendo_adicionado))
-                print('\nVoltando a tela ao Menu de Operações') #Mostra o texto antes de voltar ao menu de operações
-
-            #Caso o primeiro if não for verdadeiro(True), Verifica se o valor da variavel opção é '2', Utiliznado elif
-            elif opção == '2':
-                print('---Preparando Lista de Estudantes---\n')
-                #Verifica se a lista está vazia utilizando uma condição junto da função len na lista[0] da matriz, se estiver vazia mostra a mensegem ("Não há estudantes cadastrados")
-                if len(faculdade[0]) == 0 :
-                    print('Não há estudantes cadastrados')
-                #Caso a Lista não esteja vazia, Vai passar por cada nome da lista[0] adicionando um "-" antes do nome, e mostrando cada nome em sequência que foi adicionado
-                else:
-                    for nome in faculdade[0]:
-                        print('-',nome)
-                print('\n Voltando ao Menu de Opçoes') #Mostra o texto antes de voltar ao menu de operações
-
-            #Caso o primeiro elif não for verdadeiro(True), Verifica se o valor da variavel opção é '3'   
-            elif opção == '3':
-                print('\n EM DESENVOLVIMENTO') #Mostra a mensagem "Em desenvolvimento"
-
-            #Caso o segundo elif não for verdadeiro(True), Verifica se o valor da variavel opção é '4'
-            elif opção == '4':
-                print('\n EM DESENVOLVIMENTO') #Mostra a mensagem "Em desenvolvimento"
-
-            #Caso o terceiro elif não for verdadeiro(True), Verifica se o valor da variavel opção é '0'
-            elif (opção == '0') :
-                break   #Quebra a Estrutura de repetição(While), Fazendo voltar ao Menu Principal
-
-    #Verifica se o valor da variavel "menu_princ" é igual a '2', utilizando o if junto de uma estrutura de condição             
-    if (menu_princ == '2'):
-        while True:
-            input('\n EM DESENVOLVIMENTO \n Pressione ENTER Para Voltar ') #Mostra o Texto "EM DESENVOLVIMENTO"
-            break #Quebra a Estrutura de repetição(While), Fazendo voltar ao Menu Principal
-  
-    if (menu_princ == '3'):
-        while True: 
-            input('\n EM DESENVOLVIMENTO \n Pressione ENTER Para Voltar ') #Mostra o Texto "EM DESENVOLVIMENTO"
-            break #Quebra a Estrutura de repetição(While), Fazendo voltar ao Menu Principal
+            opção = int(input(' \n ----[Estudantes]---- Menu de Modificações \n (1) Incluir \n (2) Listar \n (3) Atualizar \n (4) Excluir \n (0) Voltar ao Menu Principal \n Opção Selecionada: '))
             
-    if (menu_princ == '4'):
-        while True:
-            input('\n EM DESENVOLVIMENTO \n Pressione ENTER Para Voltar ') #Mostra o Texto "EM DESENVOLVIMENTO"
-            break #Quebra a Estrutura de repetição(While), Fazendo voltar ao Menu Principal
+            if opção not in [1,2,3,4,0] :
+                print('Por Favor Digite um Valor pdo menu de Operações')
 
-    if (menu_princ == '5'):
-        while True:
-            input('\n EM DESENVOLVIMENTO \n Pressione ENTER Para Voltar ') #Mostra o Texto "EM DESENVOLVIMENTO"
-            break #Quebra a Estrutura de repetição(While), Fazendo voltar ao Menu Principal
+            if opção == 1:
+                print('---Incluir Estudante---')
+                estudante_sendo_adicionado = input('Qual Seria o Nome Do Estudante?')
+                if len(estudante_sendo_adicionado) == 0 :
+                        print('Nome Invalido, Voltando ao menu de Operações')
+                else:        
+                    with sqlite3.connect('Estudantes.db') as conexao:
+                        cursor = conexao.cursor()
+                        cursor.execute(''' INSERT INTO Estudantes (nome) VALUES ('{}') ''' .format(estudante_sendo_adicionado))
+                        conexao.commit
+      
+                        print('\nEstudante {} Adicionado'.format(estudante_sendo_adicionado))
+                    input("Pressione ENTER para voltar ao menu de operações") 
+                       
 
-    if (menu_princ == '8') :
-        break #Quebra a Estrutura de repetição(While), Finalizando o Programa 
+            elif opção == 2:
+                print('---Preparando Lista de Estudantes---')
+                with sqlite3.connect('Estudantes.db') as conexao:
+                    cursor = conexao.cursor()
+                    cursor.execute(''' SELECT * FROM Estudantes ''')
+                    tabela = cursor.fetchall()
+                    for linha in tabela:
+                        print('-',linha)
+                input("Pressione ENTER para voltar ao menu de operações")
+
+            elif opção ==3:
+                 with sqlite3.connect('Estudantes.db') as conexao:
+                    cursor = conexao.cursor()
+                    cursor.execute(''' SELECT * FROM Estudantes ''')
+                    tabela = cursor.fetchall()
+                    for linha in tabela:
+                        print('-',linha)
+
+                 with sqlite3.connect('Estudantes.db') as conexao :
+                    update_aluno = str(input('Qual Nome Será Colocado no lugar do outro?'))
+                    update_id = int(input('Qual ID Do outro aluno??'))
+                    cursor = conexao.cursor()
+                    cursor.execute(''' UPDATE Estudantes SET nome='{}' WHERE id={} '''.format(update_aluno,update_id))
+                    conexao.commit()
+                    print('Estudante {} Adicionado com Sucesso'.format(update_aluno))
+                    input("Pressione ENTER para voltar ao menu de operações")
+                
+            elif opção == 4:
+                with sqlite3.connect('Estudantes.db') as conexao:
+                    cursor = conexao.cursor()
+                    cursor.execute(''' SELECT * FROM Estudantes ''')
+                    tabela = cursor.fetchall()
+                    for linha in tabela:
+                        print('-',linha)
+
+                with sqlite3.connect('Estudantes.db') as conexao :
+                    delete_id = int(input('Qual ID Do outro aluno??'))
+                    cursor = conexao.cursor()
+                    cursor.execute(''' SELECT nome FROM Estudantes WHERE id={} '''.format(delete_id))
+                    deletado = cursor.fetchone()
+                    cursor.execute(''' DELETE FROM Estudantes WHERE id={} '''.format(delete_id))
+                    conexao.commit()
+                    print('Estudante {} Removido com Sucesso'.format(deletado))
+                    input("Pressione ENTER para voltar ao menu de operações")
+                
+
+            elif (opção == 0) :
+                break
+
+    if (menu_princ == 2):
+        print(' EM DESENVOLVIMENTO')
+        input(" Aperte a tecla ENTER para voltar")
+               
+    if (menu_princ == 3):
+        print(' EM DESENVOLVIMENTO')
+        input(" Aperte a tecla ENTER para voltar")
+            
+    if (menu_princ == 4):
+        print(' EM DESENVOLVIMENTO')
+        input(" Aperte a tecla ENTER para voltar")
+
+    if (menu_princ == 5):
+        print(' EM DESENVOLVIMENTO')
+        input(" Aperte a tecla ENTER para voltar")
+
+    if (menu_princ == 8) :
+        break
+    
+    
+
 
 print('Finalizando Aplicação...')

fc2c8b6d8086172a7f7e10f0c8e1db8ead2de6cd
gugahali <halickigustavo@gmail.com>
2025-04-07 13:55:23 -0300

    Termino da configuraçao das classes e integrar com a interface

--- Atividade_formativa Semana 4.py
+++ Atividade_formativa Semana 4.py
@@ -6,24 +6,49 @@ class Registro_De_Alunos:
             self.connect = sqlite3.connect('Estudantes.db')
             self.c = self.connect.cursor()
             self.create_table()
-        
-        def criar_tabela(self):
-            self.c.execute(''' CREATE TABLE IF NOT EXIST Alunos(
+
+        def create_table(self):
+            self.c.execute(''' CREATE TABLE IF NOT EXISTS Alunos(
                                 id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
                                 nome TEXT NOT NULL,
                                 turma TEXT NOT NULL,
-                                matricula TEXT NOT NULL
-                                telefone INT NOT NULL)''')
+                                matricula TEXT NOT NULL,
+                                telefone TEXT NOT NULL,
+                                email TEXT NOT NULL)''')
 
-        def registrar_aluno(self,Alunos):
-            self.c.execute(''' INSERT INTO Alunos(nome, turma, matricula, telefone) VALUES (?,?,?,?)'''(Alunos))
+        def register_student(self,Alunos):
+            self.c.execute(''' INSERT INTO Alunos(nome, turma, matricula, telefone,email) VALUES (?,?,?,?,?)''', (Alunos))
             self.connect.commit
 
             messagebox.showinfo('Adicionado com Sucesso','Registrado com sucesso')
+        
+        def Students_data(self):
+            self.c.execute(''' SELECT * FROM Alunos ''')
+            dados_alunos= self.c.fetchall()
+            for linha in dados_alunos:
+                print(f'ID: {linha[0]} | Nome: {linha[1]} | Turma: {linha[2]} | Matricula: {linha[3]} | Tel: {linha[4]} | Email: {linha[5]}')
+
+        def search_student(self,id):
+            self.c.execute(''' SELECT * FROM Alunos WHERE id = ? ''',(id,))
+            dados = self.c.fetchone()
+            
+            print(f'ID: {dados[0]} | Nome: {dados[1]} | Turma: {dados[2]} | Matricula: {dados[3]} | Tel: {dados[4]} | Email: {dados[5]}')
+
+        def update_student(self,new_values):
+            query = ''' UPDATE Alunos SET nome=? turma=? matricula=? tel=? email=? WHERE id=? '''
+            self.c.execute(query,new_values)
+            self.connect.commit()
+
+            messagebox.showinfo('Sucesso',f'O Estudante com ID:{new_values[8]} Foi Atualizado')
+        
+        def delete_student(self,id):
+            self.c.execute(''' DELETE FROM Estudantes WHERE id=? ''', (id,))
+            self.connect.commit()
+            messagebox.showinfo('Sucesso',f'O Estudante com ID:{id} Foi Deletado')
+
+sistema_de_registro = Registro_De_Alunos()
 
 
-conexao = sqlite3.connect('Estudantes.db')
-cursor = conexao.cursor()
 
 faculdade = [[],[],[],[],[]]
 
@@ -42,25 +67,25 @@ while True :
                 print('Por Favor Digite um Valor pdo menu de Operações')
 
             if opção == 1:
-                print('---Incluir Estudante---')
-                estudante_sendo_adicionado = input('Qual Seria o Nome Do Estudante?')
-                if len(estudante_sendo_adicionado) == 0 :
-                        print('Nome Invalido, Voltando ao menu de Operações')
-                else:        
-                    with sqlite3.connect('Estudantes.db') as conexao:
-                        cursor = conexao.cursor()
-                        cursor.execute(''' INSERT INTO Estudantes (nome) VALUES ('{}') ''' .format(estudante_sendo_adicionado))
-                        conexao.commit
-      
-                        print('\nEstudante {} Adicionado'.format(estudante_sendo_adicionado))
-                    input("Pressione ENTER para voltar ao menu de operações") 
+                print('---Incluir Estudante---')       
+                nome_estudante = str(input('Qual o Nome do Estudante?'))
+                turma_estudante = str(input('Qual a Turma do Estudante?'))
+                matricula_estudante = str(input('Qual o CPF do Estudante?'))
+                tel_estudante = str(input('Qual o Telefone do Estudante?'))
+                email_estudante = str(input('Qual o Email do Estudante?'))
+                estudante_dados = (nome_estudante, turma_estudante, matricula_estudante, tel_estudante, email_estudante)
+                print(estudante_dados)
+                sistema_de_registro.register_student(estudante_dados)
+                sistema_de_registro.Students_data()
+                print('\nEstudante {} Adicionado'.format(nome_estudante))
+                input("Pressione ENTER para voltar ao menu de operações") 
                        
-
+'''
             elif opção == 2:
                 print('---Preparando Lista de Estudantes---')
                 with sqlite3.connect('Estudantes.db') as conexao:
                     cursor = conexao.cursor()
-                    cursor.execute(''' SELECT * FROM Estudantes ''')
+                    cursor.execute('' SELECT * FROM Estudantes '')
                     tabela = cursor.fetchall()
                     for linha in tabela:
                         print('\n-',linha,'\n')
@@ -69,7 +94,7 @@ while True :
             elif opção ==3:
                  with sqlite3.connect('Estudantes.db') as conexao:
                     cursor = conexao.cursor()
-                    cursor.execute(''' SELECT * FROM Estudantes ''')
+                    cursor.execute('' SELECT * FROM Estudantes '')
                     tabela = cursor.fetchall()
                     for linha in tabela:
                         print('-',linha)
@@ -78,7 +103,7 @@ while True :
                     update_aluno = str(input('Qual Nome Será Colocado no lugar do outro?'))
                     update_id = int(input('Qual ID Do outro aluno??'))
                     cursor = conexao.cursor()
-                    cursor.execute(''' UPDATE Estudantes SET nome='{}' WHERE id={} '''.format(update_aluno,update_id))
+                    cursor.execute('' UPDATE Estudantes SET nome='{}' WHERE id={} ''.format(update_aluno,update_id))
                     conexao.commit()
                     print('Estudante {} Adicionado com Sucesso'.format(update_aluno))
                     input("Pressione ENTER para voltar ao menu de operações")
@@ -86,7 +111,7 @@ while True :
             elif opção == 4:
                 with sqlite3.connect('Estudantes.db') as conexao:
                     cursor = conexao.cursor()
-                    cursor.execute(''' SELECT * FROM Estudantes ''')
+                    cursor.execute('' SELECT * FROM Estudantes '')
                     tabela = cursor.fetchall()
                     for linha in tabela:
                         print('-',linha)
@@ -94,9 +119,9 @@ while True :
                 with sqlite3.connect('Estudantes.db') as conexao :
                     delete_id = int(input('Qual ID Do outro aluno??'))
                     cursor = conexao.cursor()
-                    cursor.execute(''' SELECT nome FROM Estudantes WHERE id={} '''.format(delete_id))
+                    cursor.execute('' SELECT nome FROM Estudantes WHERE id={} ''.format(delete_id))
                     deletado = cursor.fetchone()
-                    cursor.execute(''' DELETE FROM Estudantes WHERE id={} '''.format(delete_id))
+                    cursor.execute('' DELETE FROM Estudantes WHERE id={} ''.format(delete_id))
                     conexao.commit()
                     print('Estudante {} Removido com Sucesso'.format(deletado))
                     input("Pressione ENTER para voltar ao menu de operações")
@@ -128,3 +153,4 @@ while True :
 
 
 print('Finalizando Aplicação...')
+'''
